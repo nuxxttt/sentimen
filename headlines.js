7995fe032a8432406f83209d16f26eed2d3c5112
fariq <tonykriss2323@gmail.com>
2023-11-19 18:20:32 +0700

    first

---
+++ headlines.js
@@ -0,0 +1,256 @@
+const data = [
+    {
+        "headline": "Anies Pasti Canggung Mengelola Militer?",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Jusuf Kalla Keliru Soal Jokowi?",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Mengapa Anies-Imin Sok “Timnas”?",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Apakah PDIP “Terbebani” Megawati?",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Muncul di Tiap Pilpres, Isu Freeport Akan Bermain di 2024?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "PKS, Partai Terdepan Akomodasi Caleg Perempuan?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Mungkinkah Prabowo-Gibran Menang Satu Putaran?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Buzzword AI Calon Panglima Agus?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Gagasan Gibran yang Perlu Diwaspadai Capres Lain",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Menguak Rothschild dalam Perang Israel-Palestina",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Anies Paling Diuntungkan Kisruh PDIP-Jokowi?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Semua BBM Non-Subsidi Kompak Naik",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Harga Beras Naik, Porsi Warteg Berkurang?",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Laga Imin vs Mahfud vs Khofifah",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Jokowi Presiden Terkuat Sepanjang Sejarah Indonesia?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Misteri Swing Voters, Pasti Pilih Ini?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Kenapa Prabowo-Gibran Butuh Influencer?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Simpatisan Palestina, Modal Politik Anies?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Ditinggalkan Jokowi, PDIP Mainkan “Politik Teraniaya”?",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Pantaskah Susi Jadi Rebutan Para Capres?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Mengapa PDIP Tak Berani Ultimatum Jokowi?",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Pilpres 2024 adalah Pertarungan Jawa Timur?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Anies vs Prabowo, Perang Wibu 2024?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Tantangan PKS Atasi Sentimen Konservatif",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Monas Kembali Jadi Panggung Anies?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Skenario Kemenangan Tak Terbantahkan Prabowo",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Saudi Bakal Terseret Perang Israel-Palestina?",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Simbol Semangka Tidak Efektif Bela Palestina?",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Benarkah Erick “Musuh Dalam Selimut”?",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Panglima TNI Dijabat Agus Subiyanto, TNI Intervensi Pilpres 2024?",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Israel, Tanda Kejatuhan Amerika?",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Gibran Memang Seharusnya Jadi Cawapres",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Buzzer Ganjar Offside-nya Kejauhan?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Sudah Waktunya Megawati Pensiun?",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Yang Terjadi Jika Uni Soviet Masih Ada",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Kenapa Jokowi Pilih Dudung Gantikan Budi Gunawan?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Gibran Buat PDIP Terancam di Jateng?",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Prabowo vs Ganjar, Rahasia Kompetisi Arsjad-Rosan?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Wajar Saja Jokowi Cawe-cawe?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Kok Cak Imin Suka Jedag-jedug?",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Jokowi Sangat Percaya Diri Kalahkan Megawati?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Pilih Gibran Jamin Kemenangan Prabowo",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Mengapa Jokowi Semakin “Berani”?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Amerika Serikat Dukung Anies di 2024?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Kepala BIN, Keberpihakan Dudung Diuji?",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Gibran Lebih Baik Dibanding Megawati?",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Bisakah PDIP Kudeta Jokowi?",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Pilpres 2024: Pertarungan Jokowi Melawan Megawati",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Menebak Konglomerat di Belakang Anies",
+        "isPositive": true
+      },
+      
+      {
+        "headline": "Psikologis Politisi dan Pembunuh Berbeda Tipis?",
+        "isPositive": false
+      },
+      
+      {
+        "headline": "Menang, Skenario Rahasia Anies dan TNI-Polri?",
+        "isPositive": true
+      }
+      ]
